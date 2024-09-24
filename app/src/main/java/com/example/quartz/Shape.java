8181204510ef808642d1229983e191a1b2f23013
Francis Garcia <fgarcia@ualberta.ca>
2024-09-24 16:04:35 -0600

    Lab 4 Team Participation

--- app/src/main/java/com/example/quartz/Shape.java
+++ app/src/main/java/com/example/quartz/Shape.java
@@ -4,6 +4,7 @@ public abstract class Shape {
     // Fields for x and y coordinates
     private int x;
     private int y;
+    protected String color = "blue"; // $PUT_YOUR_CHOICE_OF_COLOR
 
     // Constructor to initialize x and y
     public Shape(int x, int y) {
