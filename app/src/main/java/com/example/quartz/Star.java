b90f08b908cf99bc4f13dd1d1418d0f6d1f64719
Afnan Jime <afnanjime123@gmail.com>
2024-09-24 18:58:49 -0600

    Star by afnan

---
+++ app/src/main/java/com/example/quartz/Star.java
@@ -0,0 +1,22 @@
+package quartz.app.src.main.java;
+
+public class Star extends Shape {
+        // Field for the side length of the square
+    private double sideLength;
+
+    // Constructor to initialize x, y, and sideLength
+    public Star(int x, int y, double sideLength) {
+        super(x, y); // Calling the parent class constructor
+        this.sideLength = sideLength;
+    }
+
+    // Getter for the side length
+    public double getSideLength() {
+        return sideLength;
+    }
+
+    // Setter for the side length
+    public void setSideLength(double sideLength) {
+        this.sideLength = sideLength;
+    }
+}
