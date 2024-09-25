35d5580a5da8083a8277224e83796a378b77ff81
“aaaamer1” <aaamer1@gmail.com>
2024-09-24 18:30:22 -0600

    Diamond

---
+++ app/src/main/java/com/example/quartz/Diamond.java
@@ -0,0 +1,23 @@
+package quartz.app.src.main.java;
+
+public class Diamond extends Shape{
+
+    private double sideLength; //initialize a double for side length of shape
+
+
+    public Diamond(int x, int y, double sideLength){
+        super(x,y); // parent class constructor 
+        this.sideLength = sideLength;
+        
+    }
+    
+
+    public double getSideLength(){ //getter sidelength
+        return sideLength;
+    }
+
+    public void setSideLength(double sideLength){//setter sidelength
+        this.sideLength = sideLength;
+    }
+
+}
