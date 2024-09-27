2453b6294ecff6e2ff06257775501a5f3d3334d0
Afnan Jime <afnanjime123@gmail.com>
2024-09-27 13:36:56 -0600

    adding color to shape class

--- app/src/main/java/com/example/quartz/Circle.java
+++ app/src/main/java/com/example/quartz/Circle.java
@@ -1,6 +1,6 @@
 public class Circle extends Shape {
     // Field for the side length of the square
-    private double sideLength;
+    private double sideLength; 
 
     // Constructor to initialize x, y, and sideLength
     public Circle(int x, int y, double sideLength) {
